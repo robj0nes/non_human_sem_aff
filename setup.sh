bd19c307a65706c34529576f9d0de0ae19b45a18
Rob Jones <rob@casmtechnology.com>
2026-01-28 12:51:45 +0000

    setup script update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 pip install torch torchvision numpy
-pip install git+https://github.com/IDEA-Research/GroundingDINO.git --no-build-isolation
+git clone https://github.com/IDEA-Research/GroundingDINO.git
+cd GroundingDINO/
+pip install -e .
+mkdir weights
+cd weights
+wget -q https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth
+cd ../..
 pip install .
